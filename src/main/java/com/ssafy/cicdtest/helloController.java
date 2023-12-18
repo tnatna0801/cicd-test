88553f80783355b898f2a395ac2101866e81850a
tnatna0801 <min120926@gmail.com>
2023-12-18 22:56:33 +0900

    [feat] ci: test

---
+++ src/main/java/com/ssafy/cicdtest/helloController.java
@@ -0,0 +1,13 @@
+package com.ssafy.cicdtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class helloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello.html";
+    }
+}
